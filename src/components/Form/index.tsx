99e61693e680890f832dca92415de4032e6cddda
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2024-08-19 20:27:36 +0500

    add functional fetch

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -8,13 +8,21 @@ import {
   IconButton,
   InputAdornment,
 } from '@mui/material';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
 
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import { FormValues, Schema } from '../../utils/validationShema';
 
 import styles from './Form.module.css';
+
+const toURLSearchParams = (data: FormValues) => {
+  const params = new URLSearchParams();
+  params.append('email', data.email);
+  params.append('password', data.password);
+  return params;
+};
 
 export const Form = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -32,9 +40,32 @@ export const Form = () => {
     mode: 'onChange',
   });
 
-  const onSubmit: SubmitHandler<FormValues> = () => {
-    navigate(`/`);
-    reset();
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    try {
+      const params = toURLSearchParams(data);
+
+      const authHeader = 'Basic ' + btoa('test@test.test:123321');
+
+      const response = await axios.post(
+        'https://gps.autotracker.group/api/session',
+        params,
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: authHeader,
+          },
+        },
+      );
+
+      if (response.status === 200) {
+        navigate(`/`);
+        reset();
+      } else {
+        console.error('Ошибка при создании сессии:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Произошла ошибка:', error);
+    }
   };
 
   return (
@@ -126,3 +157,132 @@ export const Form = () => {
     </form>
   );
 };
+
+// import { useState } from 'react';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import { SubmitHandler, useForm } from 'react-hook-form';
+// import {
+//   TextField,
+//   Button,
+//   Box,
+//   IconButton,
+//   InputAdornment,
+// } from '@mui/material';
+// import { Visibility, VisibilityOff } from '@mui/icons-material';
+
+// import { useNavigate } from 'react-router-dom';
+
+// import { FormValues, Schema } from '../../utils/validationShema';
+
+// import styles from './Form.module.css';
+
+// export const Form = () => {
+//   const [showPassword, setShowPassword] = useState(false);
+//   const navigate = useNavigate();
+
+//   const handleClickShowPassword = () => setShowPassword(!showPassword);
+
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors, isValid },
+//     reset,
+//   } = useForm<FormValues>({
+//     resolver: yupResolver(Schema),
+//     mode: 'onChange',
+//   });
+
+//   const onSubmit: SubmitHandler<FormValues> = () => {
+//     navigate(`/`);
+//     reset();
+//   };
+
+//   return (
+//     <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+//       <Box
+//         component='fieldset'
+//         sx={{
+//           display: 'flex',
+//           flexDirection: 'column',
+//           width: '300px',
+//           margin: 'auto',
+//           mt: 20,
+//         }}
+//       >
+//         <TextField
+//           label='Email *'
+//           variant='outlined'
+//           margin='normal'
+//           fullWidth
+//           sx={{
+//             '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+//               borderColor: '#ff9800',
+//             },
+//             '& .MuiInputLabel-root.Mui-focused': {
+//               color: '#ff9800',
+//             },
+//           }}
+//           {...register('email')}
+//         />
+//         {errors.email && (
+//           <span className={styles.formError}>{errors.email.message}</span>
+//         )}
+
+//         <TextField
+//           label='Пароль *'
+//           type={showPassword ? 'text' : 'password'}
+//           variant='outlined'
+//           margin='normal'
+//           fullWidth
+//           sx={{
+//             '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+//               color: '#ff9800',
+//               borderColor: '#ff9800',
+//             },
+//             '& .MuiInputLabel-root.Mui-focused': {
+//               color: '#ff9800',
+//             },
+//           }}
+//           InputProps={{
+//             endAdornment: (
+//               <InputAdornment position='end'>
+//                 <IconButton
+//                   title='Показать / скрыть пароль'
+//                   onClick={handleClickShowPassword}
+//                   edge='start'
+//                 >
+//                   {showPassword ? <VisibilityOff /> : <Visibility />}
+//                 </IconButton>
+//               </InputAdornment>
+//             ),
+//           }}
+//           {...register('password')}
+//         />
+//         {errors.password && (
+//           <span className={styles.formError}>{errors.password.message}</span>
+//         )}
+
+//         <Button
+//           disabled={!isValid}
+//           type='submit'
+//           variant='contained'
+//           sx={{
+//             mt: 2,
+//             backgroundColor: 'rgb(133, 95, 38)',
+//             color: '#fff',
+//             padding: '10px 20px',
+//             '&:hover': {
+//               backgroundColor: 'rgb(93, 66, 26)',
+//             },
+//             '&:disabled': {
+//               backgroundColor: 'rgba(0, 0, 0, 0.12)',
+//             },
+//           }}
+//           fullWidth
+//         >
+//           ВХОД
+//         </Button>
+//       </Box>
+//     </form>
+//   );
+// };
